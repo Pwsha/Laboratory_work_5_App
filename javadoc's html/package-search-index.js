8d02324b13933ac4c7f4342c775e09e6a4aea5df
Pwsha <pavlik.yakovlev.2021@list.ru>
2026-03-07 19:41:28 +0300

    v1.4(javadoc's files)

---
+++ javadoc's html/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.example"},{"l":"org.example.command"},{"l":"org.example.command.list"},{"l":"org.example.data"},{"l":"org.example.program"}];updateSearchResults();
